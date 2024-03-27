3ccf282deafc29d3c5ec86112fb7d550ba6f9d18
manso0109 <manso_01090@outlook.com>
2024-03-28 01:48:15 +0200

    fix

--- 3-task-manager-tutorial/starter/db/connect.js
+++ 3-task-manager-tutorial/starter/db/connect.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose')
 
 const connectDB = (url , user , pass)=> {
     mongoose.connect(url,{
+        authSource: "admin",
         user:user,
-        pass: pass
+        pass:pass
     }).then(()=> console.log('connected to DB'))
 }
 
